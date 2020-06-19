47849a17a9dc734244663a51159817dd166303ba
imperative-genre-4923 <dreama.lin@yale.edu>
2020-06-19 00:53:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
